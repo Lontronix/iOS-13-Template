e059596332a8a6f861017ab71b93754649c8077a
Lonnie Gerol <lonnie@lonniegerol.com>
2022-01-18 09:31:00 -0500

    Initial Commit

---
+++ iOS-13-Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-13-Template
+//
+//  Created by Lonnie Gerol on 1/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
